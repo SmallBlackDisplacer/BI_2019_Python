774663b2856fb470f5212173792424d929827597
Anna K <tanarri.nabassu@gmail.com>
2019-09-08 04:50:36 +0300

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+
+first_num = float(input('Enter first number: '))
+operator = input('Enter operator (+ or -): ')
+second_num = float(input('Enter second number: '))
+
+if operator == '+':
+	print (first_num + second_num)
+elif operator == '-':
+	print (first_num - second_num)
