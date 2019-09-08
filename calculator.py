7e1812c12ff04c1d4b6b0b73f806442bfcb13c5c
Anna K <tanarri.nabassu@gmail.com>
2019-09-08 04:55:25 +0300

    Add a function that validates input

--- calculator.py
+++ calculator.py
@@ -1,7 +1,19 @@
 
-first_num = float(input('Enter first number: '))
+
+def class_checker ():
+	numb = input('Enter a number: ')
+	try:
+		if '.' in numb:
+			return float(numb)
+		else:
+			return int(numb)
+	except ValueError:
+		print ("Not a number! Please try again")
+		return class_checker()
+
+first_num = class_checker()
 operator = input('Enter operator (+ or -): ')
-second_num = float(input('Enter second number: '))
+second_num = class_checker()
 
 if operator == '+':
 	print (first_num + second_num)
