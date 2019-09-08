419529a9a225715242243d217fa6b453c5069f64
Anna K <tanarri.nabassu@gmail.com>
2019-09-08 05:15:28 +0300

    Add operator validation and new operators * and /

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 
-
+# Class_checker asks for a number to enter, then returns int for int, float for float, and, if string inputed, gives you an enother chance.
 def class_checker ():
 	numb = input('Enter a number: ')
 	try:
@@ -11,11 +11,25 @@ def class_checker ():
 		print ("Not a number! Please try again")
 		return class_checker()
 
+# Operator_checker asks for operator to enter. If you make a mistake, you should try again.
+def operator_checker ():
+	op = input('Enter operator (+, -, * or /): ')
+	opers = {'+', '-', '*', '/'}
+	if op not in opers:
+		print ("Incorrect operator! Please try again")
+		return operator_checker ()
+	else:
+		return op
+
 first_num = class_checker()
-operator = input('Enter operator (+ or -): ')
+operator = operator_checker ()
 second_num = class_checker()
 
 if operator == '+':
 	print (first_num + second_num)
 elif operator == '-':
 	print (first_num - second_num)
+elif operator == '*':
+	print (first_num * second_num)
+elif operator == '/':
+	print (first_num / second_num)
