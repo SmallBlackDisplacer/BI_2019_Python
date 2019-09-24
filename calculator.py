de054979d8ed9f7099f7c95954cd1aa8d7b6817f
Anna K <tanarri.nabassu@gmail.com>
2019-09-24 19:40:45 +0300

    Fix calculator (pep8)

--- calculator.py
+++ calculator.py
@@ -1,6 +1,5 @@
-
 # Class_checker asks for a number to enter, then returns int for int, float for float, and, if string inputed, gives you an enother chance.
-def class_checker ():
+def class_checker():
     numb = input('Enter a number: ')
     try:
         if '.' in numb:
@@ -8,28 +7,30 @@ def class_checker ():
         else:
             return int(numb)
     except ValueError:
-        print ("Not a number! Please try again")
+        print("Not a number! Please try again")
         return class_checker()
 
+
 # Operator_checker asks for operator to enter. If you make a mistake, you should try again.
-def operator_checker ():
+def operator_checker():
     op = input('Enter operator (+, -, * or /): ')
     opers = {'+', '-', '*', '/'}
     if op not in opers:
-        print ("Incorrect operator! Please try again")
-        return operator_checker ()
+        print("Incorrect operator! Please try again")
+        return operator_checker()
     else:
         return op
 
+
 first_num = class_checker()
-operator = operator_checker ()
+operator = operator_checker()
 second_num = class_checker()
 
 if operator == '+':
-    print (first_num + second_num)
+    print(first_num + second_num)
 elif operator == '-':
-    print (first_num - second_num)
+    print(first_num - second_num)
 elif operator == '*':
-    print (first_num * second_num)
+    print(first_num * second_num)
 elif operator == '/':
-    print (first_num / second_num)
+    print(first_num / second_num)
