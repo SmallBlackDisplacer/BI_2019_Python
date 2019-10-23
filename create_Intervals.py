ab35ca625df2357cb0b7d0aedad45c8714b23233
Anna K <tanarri.nabassu@gmail.com>
2019-10-23 07:49:52 +0300

    Flake8 errors fixed

--- create_Intervals.py
+++ create_Intervals.py
@@ -6,11 +6,12 @@ def create_intervals_list(data):
         return []
     start = data_list[0]
     for i in range(len(data_list)):
-        if i < len(data_list)-1 and data_list[i+1] != data_list[i]+1:
+        if i < len(data_list) - 1 and data_list[i + 1] != data_list[i] + 1:
             result.append((start, data_list[i]))
-            start = data_list[i+1]
+            start = data_list[i + 1]
     result.append((start, data_list[-1]))
     return result
+
 
 # 1. Without transformation to list
 def create_intervals(data):
@@ -19,13 +20,12 @@ def create_intervals(data):
     result = []
     start, end = min(data), min(data)
     while end <= max(data):
-        if end+1 not in data and end in data:
-            result.append((start,end))
-        elif end+1 in data and end not in data:
-            start = end+1
+        if end + 1 not in data and end in data:
+            result.append((start, end))
+        elif end + 1 in data and end not in data:
+            start = end + 1
         end += 1
     return result
-
 
 
 print(create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)])
