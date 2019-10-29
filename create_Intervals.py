f820c3809eb929b4719d55b646828fa81e5ee2e8
Anna K <tanarri.nabassu@gmail.com>
2019-10-29 20:43:50 +0300

    Add blank line

--- create_Intervals.py
+++ create_Intervals.py
@@ -11,5 +11,6 @@ def create_intervals(data):
     result.append((start, data_list[-1]))
     return result
 
+
 print(create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)])
 print(create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)])
