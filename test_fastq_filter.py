9d390e04217bb43753705821c565ac65b2780085
Wojard <tanarri.nabassu@gmail.com>
2020-04-10 06:10:12 +0300

    My little testing python is magic

---
+++ test_fastq_filter.py
@@ -0,0 +1,23 @@
+import unittest
+from fastq_filter import fastq_f
+from argparse import Namespace
+import os
+
+args_d = Namespace(filename='SP1.fq', gc_bounds=[55, 60], keep_filtered=True, min_length=15,
+                   output_base_name='out_test')
+
+
+class TestFastq_f(unittest.TestCase):
+    def test_is_working(self):
+        fastq_f(args_d)
+        self.assertTrue(os.path.exists('./out_test__passed.fastq'))
+        if os.path.exists('./out_test__passed.fastq'):
+            os.remove('./out_test__passed.fastq')
+        self.assertTrue(os.path.exists('./out_test__failed.fastq'))
+        if os.path.exists('./out_test__failed.fastq'):
+            print ('hm')
+            os.remove('./out_test__failed.fastq')
+
+
+if __name__ == "__main__":
+    unittest.main()
